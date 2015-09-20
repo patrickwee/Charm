8c53f09f90a7f30f996219a3c44dcc4cd720a38c
Patrick Wee <superwee100@gmail.com>
2015-09-20 00:19:54 -0400

    fixed tags from jokes to pick up lines, command D is awesome

--- server.py
+++ server.py
@@ -46,147 +46,147 @@ texts = [
     'text': "You know what material this is? Boyfriend/Girlfriend material.",
     'ups': 0,
     'downs': 0,
-    'text_type': 'joke'
+    'text_type': 'pickup'
   },
   {
     'id': 5,
     'text': "“If I were a gardener, I’d put your tulips and my tulips together.",
     'ups': 0,
     'downs': 0,
-    'text_type': 'joke'
+    'text_type': 'pickup'
   },
   {
     'id': 6,
     'text': "Hi, you got raisins? No? Well, how about a date?",
     'ups': 0,
     'downs': 0,
-    'text_type': 'joke'
+    'text_type': 'pickup'
   },
   {
     'id': 7,
     'text': "Are you a thief? 'Cause you stole my heart!",
     'ups': 0,
     'downs': 0,
-    'text_type': 'joke'
+    'text_type': 'pickup'
   },
   {
     'id': 8,
     'text': "Are you a cliff? 'Cause I'm falling for you!",
     'ups': 0,
     'downs': 0,
-    'text_type': 'joke'
+    'text_type': 'pickup'
   },
   {
     'id': 9,
     'text': "I'm not drunk, I'm just intoxicated by you!",
     'ups': 0,
     'downs': 0,
-    'text_type': 'joke'
+    'text_type': 'pickup'
   },
   {
     'id': 10,
     'text': "Do you like going to the gym? 'Cause you've been running in my mind!",
     'ups': 0,
     'downs': 0,
-    'text_type': 'joke'
+    'text_type': 'pickup'
   },
   {
     'id': 11,
     'text': "Am I in heaven? Or do you just look like an angel?",
     'ups': 0,
     'downs': 0,
-    'text_type': 'joke'
+    'text_type': 'pickup'
   },
   {
     'id': 12,
     'text': "Your dad must be a terrorist, ‘coz yoh da bomb!",
     'ups': 0,
     'downs': 0,
-    'text_type': 'joke'
+    'text_type': 'pickup'
   },
   {
     'id': 13,
-    'text': "Was your dad an alien? ‘Coz honey, there’s nothing else like you on planet Earth!,
+    'text': "Was your dad an alien? ‘Coz honey, there’s nothing else like you on planet Earth!",
     'ups': 0,
     'downs': 0,
-    'text_type': 'joke'
+    'text_type': 'pickup'
   },
   {
     'id': 14,
     'text': "Hi, you must be the devil? ‘Coz you’re hot as hell!",
     'ups': 0,
     'downs': 0,
-    'text_type': 'joke'
+    'text_type': 'pickup'
   },
   {
     'id': 15,
     'text': "Life without you would be like a broken pencil… pointless.",
     'ups': 0,
     'downs': 0,
-    'text_type': 'joke'
+    'text_type': 'pickup'
   },
   {
     'id': 16,
     'text': "Hi, I know I’m a guy but I want to be Alice, coz your body’s a Wonderland.",
     'ups': 0,
     'downs': 0,
-    'text_type': 'joke'
+    'text_type': 'pickup'
   },
   {
     'id': 17,
     'text': "Are you a keyboard? 'Cause you're my type!",
     'ups': 0,
     'downs': 0,
-    'text_type': 'joke'
+    'text_type': 'pickup'
   },
   {
     'id': 18,
     'text': "Top view, side view, bottom view, whatever view, I love view.",
     'ups': 0,
     'downs': 0,
-    'text_type': 'joke'
+    'text_type': 'pickup'
   },
   {
     'id': 19,
     'text': "Are you good in algebra? Can you substitute my ‘x’?",
     'ups': 0,
     'downs': 0,
-    'text_type': 'joke'
+    'text_type': 'pickup'
   },
   {
     'id': 20,
     'text': "Are you pizza? Because I have a ‘crust’ on you.",
     'ups': 0,
     'downs': 0,
-    'text_type': 'joke'
+    'text_type': 'pickup'
   },
   {
     'id': 21,
     'text': "Can I take your picture? 'Cause I wanna show Santa what I want for Christmas.",
     'ups': 0,
     'downs': 0,
-    'text_type': 'joke'
+    'text_type': 'pickup'
   },
   {
     'id': 22,
     'text': "Are you a dictionary? Because you give meaning to my life.",
     'ups': 0,
     'downs': 0,
-    'text_type': 'joke'
+    'text_type': 'pickup'
   },
   {
     'id': 23,
     'text': "You must be in a wrong place - the Miss Universe contest is over there.",
     'ups': 0,
     'downs': 0,
-    'text_type': 'joke'
+    'text_type': 'pickup'
   },
   {
     'id': 24,
     'text': "You must be a banana because I find you a peeling.",
     'ups': 0,
     'downs': 0,
-    'text_type': 'joke'
+    'text_type': 'pickup'
   }
 ]
 
